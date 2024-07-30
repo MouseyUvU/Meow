c85d4c778b8ea7166483f7ed242f31dad5a48e66
Mousey <138757576+MouseyUvU@users.noreply.github.com>
2024-07-30 02:10:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const $img = document.getElementById('imgClickAndChange');
-const kitty_meow = 'http://localhost:5500/meow/kitty_meow.jpg';
-const silly_kitty = 'http://localhost:5500/meow/silly_kitty.jpg';
-const meow_sfx = 'http://localhost:5500/meow/Meow_sfx.mp3';
+const kitty_meow = './meow/kitty_meow.jpg';
+const silly_kitty = './meow/silly_kitty.jpg';
+const meow_sfx = './meow/Meow_sfx.mp3';
 
 function changeImage() {
-    console.log($img.src)
-    if ($img.src == kitty_meow){
+    console.log($img.dataset.kitty)
+    if ($img.dataset.kitty == "meow"){
+        $img.dataset.kitty = "silly";
         $img.src = silly_kitty;
     } else {
+        $img.dataset.kitty = "meow";
         $img.src = kitty_meow;
     }
 }
-    let Meow_sfx = new Audio();
-    Meow_sfx.src = meow_sfx
+
+let Meow_sfx = new Audio();
+Meow_sfx.src = meow_sfx
     const el = document.createElement('audio')
     
 $img.addEventListener('click', () => {
