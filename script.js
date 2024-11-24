db43c3c8f2bbbdaa184d162f5f4607ef6a9bd403
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-11-24 11:34:26 -0600

    refactor: to improve audio handling

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ const kitty_meow = 'kitty_meow.jpg';
 const silly_kitty = 'silly_kitty.jpg';
 const meow_sfx = 'Meow_sfx.mp3';
 
-function changeImage() {
-    console.log($img.dataset.kitty)
-    if ($img.dataset.kitty == "meow"){
-        $img.dataset.kitty = "silly";
-        $img.src = silly_kitty;
-    } else {
-        $img.dataset.kitty = "meow";
-        $img.src = kitty_meow;
-    }
+function updateImage() {
+	if ($img.dataset.kitty === "meow") {
+		$img.dataset.kitty = "silly";
+		$img.src = silly_kitty;
+	} else {
+		$img.dataset.kitty = "meow";
+		$img.src = kitty_meow;
+	}
 }
 
-let Meow_sfx = new Audio();
-Meow_sfx.src = meow_sfx
-    const el = document.createElement('audio')
-    
+function playSound() {
+	const audio = new Audio(meow_sfx);
+	audio.play();
+}
+
 $img.addEventListener('click', () => {
-  changeImage();
-  const el = document.createElement('audio');
-  el.src = 'Meow_sfx.mp3';
-  document.body.append(el);
-  el.play();
+	updateImage();
+	playSound()
 });
